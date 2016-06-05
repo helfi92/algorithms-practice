3fa39fdff8f45f8c034fc95237325e1abbfc46f7
helfi92 <helfi92@gmail.com>
2016-06-05 15:08:19 -0700

    rand7rand5

---
+++ rand7rand5.js
@@ -0,0 +1,16 @@
+/*
+ *
+ *	Implement a method rand7() given rand5(). That is, given a method that generates
+ *	a random number between 0 and 4 (inclusive), write a method that generates a random
+ *	number between - and 6 (inclusive)
+ *
+ *
+ *
+ */
+
+function rand7(rand4) {
+	
+}
+
+var rand4 = Math.random() * 4;
+console.log(rand7(rand4));
